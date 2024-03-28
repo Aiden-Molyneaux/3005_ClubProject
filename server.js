f5cf3548d3f69542eac2b6c8bb8b9c053b057536
Aiden Molyneaux <aiden@wicket.io>
2024-03-28 14:56:41 -0400

    Add initial server auth logic

---
+++ server.js
@@ -0,0 +1,28 @@
+const passport = require("passport");
+require("./passportConfig")(passport);
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post(
+  "/auth/signup",
+  passport.authenticate("local-signup", { session: false }),
+  (req, res, next) => {
+    res.json({
+      user: req.user,
+    });
+  }
+);
+
+app.post(
+  "/auth/login",
+  passport.authenticate("local-login", { session: false }),
+  (req, res, next) => {
+    res.json({ user: req.user });
+  }
+);
+
+app.listen(3000, () => console.log("Listening on port 3000"));
